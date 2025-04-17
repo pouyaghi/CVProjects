a3162abec11c3c90de025cf0d7572ea2a4851ffe
amirpouya <pouyaghtoo@gmail.com>
2025-04-17 18:08:27 -0400

    small adjustment

--- Hand-detection/mediapipe_hand_detection.py
+++ Hand-detection/mediapipe_hand_detection.py
@@ -1,4 +1,4 @@
-"""import cv2
+import cv2
 import mediapipe as mp
 
 mp_hands = mp.solutions.hands
@@ -10,7 +10,7 @@ hands = mp_hands.Hands(static_image_mode=False,
                        min_detection_confidence=0.5,
                        min_tracking_confidence=0.5)
 
-cap = cv2.VideoCapture("http://172.29.32.1:5000/video")
+cap = cv2.VideoCapture()
 
 while True:
     success, image = cap.read()
@@ -28,43 +28,6 @@ while True:
     cv2.imshow("MediaPipe Hand Detection", image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-"""
-import cv2
-import mediapipe as mp
-
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-
-# Initialize MediaPipe Hands
-hands = mp_hands.Hands(static_image_mode=False,
-                       max_num_hands=2,
-                       min_detection_confidence=0.5,
-                       min_tracking_confidence=0.5)
-
-cap = cv2.VideoCapture("http://172.29.32.1:5000/video")
-
-while True:
-    success, image = cap.read()
-    if not success:
-        break
-
-    image = cv2.flip(image, 1)  # Flip for selfie-view
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image_rgb)
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-    # Display the resulting frame with OpenCV
-    cv2.imshow("MediaPipe Hand Detection", image)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
         break
 
 cap.release()
